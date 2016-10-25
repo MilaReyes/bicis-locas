53ef8f68af77a7dddc1dc27dde501af3c8b6acbd
milareyes <milena.reyes@gmail.com>
2016-10-25 18:18:39 -0300

    Campo nombre

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+  var name=document.getElementById('name').value;
+    //var lastName=document.getElementById('lastname').value;
+    //var mail=document.getElementById('input-email').value;
+    //var password=document.getElementById('input-password').value;
+    
+    if( name == null || name.length <=2 || /^\s+$/.test(name) || /^[a-zA-Z]*$/.test(name)==false) {
+        alert("Ingresa un nombre valido!");
+        return false;
+}
 }
